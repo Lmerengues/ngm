a804789e182400694c673f2e069ad20994c2534e
lmerengues <317958662@qq.com>
2017-12-16 21:56:17 +0800

    test

--- ngm/view.py
+++ ngm/view.py
@@ -230,14 +230,18 @@ def find_path(request):
     if(len(datap) < 10):
         step = 3
         result_len = 0
-        while(result_len<10):
-            mydata = test_graph.run("MATCH (fromNodes:Person) where fromNodes.name='"+fperson+"' MATCH (toNodes:Person) where toNodes.name='"+tperson+"' CALL apoc.algo.allSimplePaths(fromNodes, toNodes, 'know',"+str(step)+") yield path RETURN path,reduce(a=1, r in rels(path) | a+r.val) as orders ORDER BY length(path),orders desc limit 10")
+        while result_len < 10:
+            path_store = []
+            mydata = test_graph.run(
+                "MATCH (fromNodes:Person) where fromNodes.name='" + fperson + "' MATCH (toNodes:Person) where toNodes.name='" + tperson + "' CALL apoc.algo.allSimplePaths(fromNodes, toNodes, 'know'," + str(
+                    step) + ") yield path RETURN path,reduce(a=1, r in rels(path) | a+r.val) as orders ORDER BY length(path),orders desc limit 10")
             result_len = 0
             for item in mydata:
-                result_len+=1
+                result_len += 1
+                path_store.append(item['path'])
             step += 1
-        for p in mydata:
-            datap.append(p['path'])
+        for p in path_store:
+            datap.append(p)
     nodes_total = []
     rels_total = []
     #all_nodes_total = []
